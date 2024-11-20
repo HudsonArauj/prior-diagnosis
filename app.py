de948be5f034f8707d8cb809728c58878eb98946
HudsonArauj <hudsonma@al.insper.edu>
2024-11-20 19:30:07 -0300

    feat/diagnosis

---
+++ app.py
@@ -0,0 +1,133 @@
+import streamlit as st
+from llama_index.core import SimpleDirectoryReader
+from llama_parse import LlamaParse
+from utils import formated_docs, chain_structured_invoke, chain_diagnostico, convert_to_float, convert_to_int
+from PyPDF2 import PdfReader
+import os
+import pandas as pd
+from streamlit import session_state as ss
+from utils import *
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+
+st.markdown(
+    """
+    ## Gera diagnóstico de exames de sangue a partir de arquivos PDF.
+    """,
+    unsafe_allow_html=True
+)
+
+uploaded_file = st.file_uploader("Gerar diagnóstico do exame.", type=['pdf'])
+
+if uploaded_file:
+    file_path = os.path.join('.', uploaded_file.name)
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    parser = LlamaParse(
+        result_type="markdown" 
+    )
+
+    try:
+        parsing_result = parser.load_data([file_path])
+    except Exception as e:
+        st.error(f"Erro ao analisar o arquivo: {e}")
+        parsing_result = []
+
+    button_gerar_diagnostico = st.button(
+        label='Diagnóstico',
+        type='primary'
+    )
+    
+    
+    if button_gerar_diagnostico:
+        if not parsing_result:
+            st.markdown('Por favor, insira um arquivo PDF válido para continuar.')
+        else:
+            with st.spinner('Gerando diagnóstico...'):
+                
+                data = chain_structured_invoke(formated_docs(parsing_result))
+                data_dict = data.tool_calls[0]['args']
+                # replace ',' to '.' for float conversion
+                data_dict = {k: v.replace(",", ".") for k, v in data_dict.items()}
+                st.title("Resultado do Exame de Sangue")
+                st.write("## Dados do Paciente")
+                st.write(f"**Idade:** {data_dict['idade']}")
+                st.write(f"**Sexo:** {data_dict['sexo']}")
+                st.write(f"**Data do exame:** {data_dict['data']}")
+
+                st.write("## Resultados do Hemograma")
+                
+                #transformando o dicionário em um dataframe
+                cwdf = pd.DataFrame(data_dict.items(), columns=['Dados extraídos', 'Resultado'])
+                
+                plaquetas = convert_to_float(data_dict.get('plaquetas', '0'))
+                hemoglobina = convert_to_float(data_dict.get('hemoglobina', '0'))
+                neutrofilos = convert_to_float(data_dict.get('neutrofilos', '0'))
+                linfocitos = convert_to_float(data_dict.get('linfocitos', '0'))
+                vcm = convert_to_float(data_dict.get('vcm', '0'))
+                vmc = convert_to_float(data_dict.get('vmc', '0'))
+                vpm = convert_to_float(data_dict.get('vpm', '0'))
+                macroplaquetas = data_dict.get('macroplaquetas', 'False')
+                medicacao = data_dict.get('medicacao', 'False')
+                infeccao = data_dict.get('infeccao', 'False')
+                idade = convert_to_int(data_dict.get('idade', '0'))
+                leucocitos = convert_to_float(data_dict.get('leucocitos', '0'))
+                chcm = convert_to_float(data_dict.get('chcm', '0')) 
+               
+                            
+                result_diagnostico = chain_diagnostico(plaquetas, hemoglobina, neutrofilos, linfocitos, vcm, vmc, vpm, macroplaquetas, medicacao, infeccao, idade, sexo=data_dict['sexo'], data=data_dict['data'], leucocitos=leucocitos, chcm=chcm)
+
+                
+                
+                if not cwdf.empty:
+                    # Criar a tabela usando Plotly
+                    fig = go.Figure(
+                        data=[go.Table(
+                            columnorder=[1, 2], 
+                            columnwidth=[15, 40],
+                            header = dict(
+                                values = list(cwdf.columns),
+                                font=dict(size=11, color = 'white'),
+                                fill_color = '#264653',
+                                line_color = 'darkslategray',
+                                align = ['left','center'],
+                                #text wrapping
+                                height=20
+                            ),
+                            cells=dict(
+                                values=[cwdf[col][3:].tolist() for col in cwdf.columns],
+                                font=dict(size=12, color='black'),
+                                align = ['left','center'],
+                                fill_color='white',
+                                height=20
+                            )
+                        )]
+                    )
+                    
+                    fig.update_layout(
+                        title_text="Resultado",
+                        title_font_color='#264653',
+                        title_x=0,
+                        margin=dict(l=0, r=10, b=10, t=30),
+                        height=480
+                    )
+                    
+                    # Exibir a tabela no Streamlit
+                    st.plotly_chart(fig, use_container_width=True)
+                    
+                    st.title('Diagnóstico:')
+                    st.markdown(f"**Diagnóstico:** {result_diagnostico}")
+
+
+                else:
+                    st.write("Nenhum dado encontrado para exibir.")
+                
+                st.write("## Observações")
+                st.write("Os resultados do exame devem ser analisados por um médico especializado para um diagnóstico preciso.")
+    
+    # Optionally, clean up the file after processing
+    os.remove(file_path)
---
+++ utils.py
@@ -0,0 +1,137 @@
+# bring in our LLAMA_CLOUD_API_KEY
+from dotenv import load_dotenv
+# bring in deps
+import nest_asyncio
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from tqdm import tqdm
+from pydantic import BaseModel, Field
+from langchain_core.prompts import ChatPromptTemplate
+import os
+from langchain_core.output_parsers import StrOutputParser
+from langchain_pinecone import PineconeVectorStore
+
+
+nest_asyncio.apply()
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+LLAMA_CLOUD_API_KEY = os.getenv("LLAMA_CLOUD_API_KEY")
+
+"""
+
+Relevant Chains and functions
+
+"""
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+vectorstore = PineconeVectorStore.from_existing_index(
+    index_name="pf-nlp", embedding=embeddings)
+
+
+llm = ChatOpenAI(
+    model="gpt-4o-2024-08-06",  # 100% json output
+    temperature=0,
+)
+
+
+class GetSchema(BaseModel):
+    """Extrai valores relevantes de um documento de exame médico de hemograma"""
+    idade: str = Field(description="Valor numérico de idade")
+    sexo: str = Field(description="Valor de sexo")
+    data: str = Field(description="Data do exame")
+
+    hemacias: str = Field(description="Valor numérico de hemácias")
+    hemoglobina: str = Field(description="Valor numérico de hemoglobina")
+    hematocrito: str = Field(description="Valor numérico de hematocrito")
+    vcm: str = Field(description="Valor numérico de VCM")
+    hcm: str = Field(description="Valor numérico de HCM")
+    chcm: str = Field(description="Valor numérico de CHCM")
+    rdw: str = Field(description="Valor numérico de RDW")
+    plaquetas: str = Field(
+        description="Valor numérico de plaquetas em µL", examples=["10000"])
+    vmp: str = Field(description="Valor numérico de VMP")
+    vmc: str = Field(description="Valor numérico de VMC")
+    vpm: str = Field(description="Valor numérico de VPM")
+    macroplaquetas: str = Field(description="Valor booleano de macroplaquetas")
+    medicacao: str = Field(description="Valor booleano de medicamento")
+    infeccao: str = Field(description="Valor booleano de infecção")
+
+    leucocitos: str = Field(
+        description="Valor numérico de leucócitos em µL", examples=["10000"])
+    linfocitos: str = Field(
+        description="Valor numérico de linfócitos em µL", examples=["10000"])
+    monocitos: str = Field(
+        description="Valor numérico de monócitos em µL", examples=["10000"])
+    neutrofilos: str = Field(
+        description="Valor numérico de neutrófilos em µL", examples=["10000"])
+    # esquizocitos: str = Field(description="Valor booleano de esquizócitos")
+
+
+def convert_to_float(value):
+    try:
+        return float(value)
+    except:
+        return None
+
+
+def convert_to_int(value):
+    try:
+        return int(value)
+    except:
+        return None
+
+
+def chain_structured_invoke(docs):
+
+    # Prompt
+    system = """Você vai receber um documento em PDF com os resultados de um exame de hemograma e outros. Foque somente no exame de hemograma."""
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+
+            ("human", "Exame completo: {exame}"),
+        ]
+    )
+
+    llm_with_tools = llm.bind_tools([GetSchema])  # , strict=True)
+    chain_structured = prompt | llm_with_tools
+
+    response = chain_structured.invoke({"exame": docs})
+    return response
+
+
+def formated_docs(docs):
+    return "\n\n".join([doc.text for doc in docs])
+
+
+def format_docs_search(docs):
+    return "\n\n".join([doc.page_content for doc in docs])
+
+
+def chain_diagnostico(plaquetas=0, hemoglobina=0, neutrofilos=0, linfocitos=0, vcm=0, vmc=0, vpm=0, macroplaquetas=0, medicacao=0, infeccao=0, idade=0, sexo=0, data=0, leucocitos=0, chcm=0):
+    base_rule = "Você é um assistente de médico que irá ajudar montar o diagnóstico do paciente baseado no hemograma."
+    
+    query = f"O paciente tem {idade} anos, é do sexo {sexo}, e fez o exame no dia {data}. Os resultados do hemograma são: Plaquetas: {plaquetas}, Hemoglobina: {hemoglobina}, Neutrófilos: {neutrofilos}, Linfócitos: {linfocitos}, VCM: {vcm}, VMC: {vmc}, VPM: {vpm}, Macroplaquetas: {macroplaquetas}, Medicação: {medicacao}, Infecção: {infeccao}"
+    docs = vectorstore.similarity_search(query, k=10)
+    docs_str = format_docs_search(docs)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", base_rule),
+            ("system",f"O paciente tem {idade} anos, é do sexo {sexo}, e fez o exame no dia {data}."),
+            ("system", "Os resultados do hemograma são:"),
+            ("system", f"Resultado: {query}"),
+            ("system",f"Baseie o diagnóstico dos seguintes documentos de referência:\n{docs_str}"),
+            ("human", "Diagnóstico do paciente com base nos resultados do hemograma é :"),
+        ]
+    )
+    llm = ChatOpenAI(model = 'gpt-4-0125-preview')
+    chain = (
+        prompt
+        | llm
+        | StrOutputParser()
+    )
+    response = chain.invoke({})
+    return response
